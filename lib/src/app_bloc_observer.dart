988ccda95b9314bad7cb0eadd4e3da9201fae0ea
Mohit Varma <mv501049@gmail.com>
2024-01-02 22:39:26 +0530

    bloc observer method implemented.

--- lib/src/app_bloc_observer.dart
+++ lib/src/app_bloc_observer.dart
@@ -6,4 +6,34 @@ class AppBlocObserver extends BlocObserver {
     super.onCreate(bloc);
     print("Created ${bloc.state}");
   }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    // TODO: implement onTransition
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    // TODO: implement onChange
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    // TODO: implement onClose
+    super.onClose(bloc);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    // TODO: implement onEvent
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+    super.onError(bloc, error, stackTrace);
+  }
 }
